6355a9b9be09e08a59d5f29b8a824ccfadf8de14
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-04-14 21:33:22 -0300

    Radio Button feito

---
+++ app/componentes/RadioButton/index.jsx
@@ -0,0 +1,27 @@
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+export default function RadioButton({ label, selected, onPress }) {
+  return (
+    <TouchableOpacity style={estilos.radioContainer} onPress={onPress}>
+      <Ionicons
+        name={selected ? "radio-button-on" : "radio-button-off"}
+        size={20}
+        color={"#0063C7"}
+      />
+      <Text
+        style={[
+          estilos.radioLabel,
+          selected && { fontWeight: "bold", color: "#007BFF" },
+        ]}
+      >
+        {label}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const estilos = StyleSheet.create({
+  radioContainer: { flexDirection: "row", alignItems: "center" },
+  radioLabel: { marginLeft: 5, fontSize: 14, color: "#0056b3" },
+});
