f2ce3048e39a6de832893cf1e040d0d534330ee3
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-04-16 20:07:29 -0300

    arquivo js para conexão com o back end

---
+++ app/servicos/profissionais.js
@@ -0,0 +1,11 @@
+import api from "./api";
+
+export async function listarProfissionais(setProfissionais) {
+  try {
+    await api.get("/profissionais").then((resposta) => {
+      setProfissionais(resposta.data);
+    });
+  } catch (erro) {
+    Alert.alert("Erro ao listar profissionais:", erro);
+  }
+}
