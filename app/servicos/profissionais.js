39ddbd044aa520fb24731934edfb9807a67b0ae3
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-04-16 21:40:29 -0300

    função de buscar profissionais

--- app/servicos/profissionais.js
+++ app/servicos/profissionais.js
@@ -9,3 +9,14 @@ export async function listarProfissionais(setProfissionais) {
     Alert.alert("Erro ao listar profissionais:", erro);
   }
 }
+
+export async function buscarProfissional(id, setProfissional) {
+    try{
+        await api.get(`/profissionais/${id}`).then((resposta) => {
+            setProfissional(resposta.data);
+        });
+    } catch (erro) {
+        Alert.alert("Erro ao buscar profissional:", erro);
+
+    }
+}
